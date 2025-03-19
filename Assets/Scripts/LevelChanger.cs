02ac7ebaf4e97fe08bd6f3bea654e6eb44cb262e
Sashabox <71496023+Sashabox@users.noreply.github.com>
2025-03-19 22:09:19 +0300

    first commit

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class LevelChanger : MonoBehaviour
+{
+    public void ChangeLevel(string lvl = "")
+    {
+        SceneManager.LoadScene(lvl);
+    }
+}
